87e0e756353e96f7c63fad7bac60761e7f3774d2
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2023-04-17 20:34:46 +0600

    Modal

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+  
+  <SiteNavigation />
   <RouterView />
 
   </div>
@@ -8,6 +9,7 @@
 
 <script setup>
 import {RouterView} from 'vue-router'
+import SiteNavigation from './components/SiteNavigation.vue'
 
 </script>
 
