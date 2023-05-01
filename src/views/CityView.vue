1801e09df6db7df9f54f56f369a77af98223e40e
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2023-05-01 16:48:18 +0600

    City view layout

---
+++ src/views/CityView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Suspense>
+      <AsyncCityView />
+      <template #fallback>
+      <i class="fa-sharp fa-solid fa-loader animate-spin"></i>
+        <p>Loading...</p>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script setup>
+import AsyncCityView from "../components/AsyncCityView.vue";
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,6 +26,7 @@
             v-for="searchResult in mapboxSearchResults"
             :key="searchResult.id"
             class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)"
           >
             {{ searchResult.place_name }}
           </li>
@@ -38,6 +39,21 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const previewCity = (searchResult) => {
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: "cityView",
+    params: { state: state.replaceAll(" ", ""), city: city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    },
+  });
+};
 
 const mapboxAPIKey =
   "pk.eyJ1IjoibW9rYW1tZWx0YW52aXIiLCJhIjoiY2xnbDJhZnd0MGlkaDNrcXEzdGZ6bm5jNSJ9.d7tZGXn-ntjPYOBN58ZW9w";
